1429a5bccbadcf53a649e9d875377123e0c6b477
Paris <paris@crookedpath.co.uk>
2020-12-27 21:45:32 +0000

    added more files for deploying

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from models.recipe import Recipe
 from common.database import Database
 from common.scraper import Scraper
 from models.book import Book
-
+from scout_apm.flask import ScoutApm
 from flask import Flask, render_template, request, session, make_response
+
 
 app = Flask(__name__)  # '__main__'
 app.secret_key = "paris"
 
+ScoutApm(app)
+app.config["SCOUT_MONITOR"] = True
+app.config["SCOUT_KEY"] = "wtwp536w8u9DzTlDTpiy"
+app.config["SCOUT_NAME"] = "Recipe book generator"
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
