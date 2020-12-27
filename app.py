74a3d7c890e28a11706c58bebc0c4966c2a39fa6
Paris <paris@crookedpath.co.uk>
2020-12-27 23:01:03 +0000

    alterations to debug

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from models.recipe import Recipe
 from common.database import Database
-from common.scraper import Scraper
+#from common.scraper import Scraper
 from models.book import Book
-from scout_apm.flask import ScoutApm
+#from scout_apm.flask import ScoutApm
 from flask import Flask, render_template, request, session, make_response
 
 
 app = Flask(__name__)  # '__main__'
 app.secret_key = "paris"
 
-ScoutApm(app)
+"""ScoutApm(app)
 app.config["SCOUT_MONITOR"] = True
 app.config["SCOUT_KEY"] = "wtwp536w8u9DzTlDTpiy"
 app.config["SCOUT_NAME"] = "Recipe book generator"
+"""
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -29,14 +29,14 @@ def home():
 def recipe_book(your_book):
     return render_template('book.html', your_book=your_book)
 
-
+"""
 @app.before_first_request
 def initialize_database():
     Database.initialize()
-
+"""
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 """
